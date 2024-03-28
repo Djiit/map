1fc4b593338fb0cd15d2b4db4a8bdba35251451d
Julien Tanay <julien.tanay@gmail.com>
2024-03-28 22:38:14 +0100

    fix: pwa installability

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {};
 const withSerwist = withSerwistInit({
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
+  disable: process.env.NODE_ENV === "development",
 });
 
 export default withSerwist(nextConfig);
