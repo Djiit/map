8bc533c28cffddfd64b627791157fdb0184dfb9f
Julien Tanay <julien.tanay@gmail.com>
2024-03-28 22:25:07 +0100

    feat: add PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,51 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const APP_NAME = "Map";
+const APP_DEFAULT_TITLE = "My Awesome Map";
+const APP_TITLE_TEMPLATE = "%s - Map";
+const APP_DESCRIPTION = "Best Map in the world!";
 
 export const metadata: Metadata = {
-  title: "Map",
-  description: "Map",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
+const inter = Inter({ subsets: ["latin"] });
 
 export const revalidate = 3600;
 
@@ -17,7 +55,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" dir="ltr">
       <body className={inter.className}>{children}</body>
     </html>
   );
