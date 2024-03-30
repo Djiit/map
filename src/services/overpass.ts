641915da8671203a424108677608f56140c06392
Julien Tanay <julien.tanay@gmail.com>
2024-03-30 14:30:44 +0100

    fix: remove types for now

--- src/services/overpass.ts
+++ src/services/overpass.ts
@@ -5,7 +5,7 @@ import { cache } from "react";
 const api = DefaultOverpassApi();
 
 export const getData = cache(async (query: any) => {
-  const results = await api.execJson((s) => {
+  const results = await api.execJson((s: any) => {
     return [s.node.query(query)];
   });
   console.debug(`Found ${results.elements.length} elements.`);
